17e27b3873a80976073561ec112eb0e426dcb050
dev-alkadmi <196874690+dev-alkadmi@users.noreply.github.com>
2025-06-17 13:19:08 -0400

    Created logic for resetting the grid

--- drawLogic.js
+++ drawLogic.js
@@ -1,18 +1,39 @@
 const container = document.getElementById("container");
 
-for (let index = 0; index < 16; index++) {
-    const columnDiv = document.createElement('div');
-    columnDiv.classList.add("column");
-    container.append(columnDiv);
-    for (let index = 0; index <16; index++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add('row');
-        columnDiv.append(rowDiv);
+function setGrid(gridSize) {
+
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
     }
+
+    for (let index = 0; index < gridSize; index++) {
+        const columnDiv = document.createElement('div');
+        columnDiv.classList.add("column");
+        container.append(columnDiv);
+        for (let index = 0; index < gridSize; index++) {
+            const rowDiv = document.createElement('div');
+            rowDiv.classList.add('row');
+            columnDiv.append(rowDiv);
+        }
+    }
+
+
+    const gridDivs = document.querySelectorAll(('.row'));
+
+    gridDivs.forEach(el => el.addEventListener('mouseover', event => {
+        el.classList.add("color");
+    }));
 }
 
-const gridDivs = document.querySelectorAll(('.row'));
+setGrid(16);
 
-gridDivs.forEach(el => el.addEventListener('mouseover', event => {
-  el.classList.add("color");
-}));
+const gridChangeButton = document.getElementById("changeGrid");
+
+gridChangeButton.addEventListener('click', (event) => {
+    let userInput = 0;
+    do {
+        userInput = parseInt(prompt('Input Grid size less the 101 and larger then 0', "16"), 10);
+    } while (!(Number.isInteger(userInput) && userInput <= 100 && userInput > 0));
+
+    setGrid(userInput);
+});
